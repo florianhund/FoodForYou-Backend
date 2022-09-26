44f3d672ab57e48767bc00f43f0eaf9b71b12eb8
florianhund <flo.hundegger@gmail.com>
2022-09-26 22:14:23 +0200

    release: bump version to v2.0.0-alpha.2

--- src/config/docs/basicInfo.ts
+++ src/config/docs/basicInfo.ts
@@ -3,7 +3,7 @@ export default {
   info: {
     title: 'FoodForYou',
     description: 'Food Delivery App',
-    version: '2.0.0-alpha.1',
+    version: '2.0.0-alpha.2',
     contact: {
       name: 'Florian Hundegger',
       email: 'flo.hundegger@gmail.com',
