9b0e8d4c7b7ef91a40c2326f97dd1b530f528d9c
Florian Hundegger <flo.hundegger@gmail.com>
2023-02-25 11:03:38 +0100

    feat: implement script to bump version
    Implement flags to do an automatic commit

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+commit() {
+  git add .
+  git commit -m "build(release): bump version to v$version"
+}
+
+while getopts 'cv:-:' flag; do
+  case "${flag}" in
+    v) version=$OPTARG ;;
+    c) is_commit=true ;;
+    -) if [ "$OPTARG" = "commit-git" ]
+       then
+        is_commit=true
+       fi ;;
+  esac
+done
+
+if [ -z "$version" ]
 then
   echo "Fatal! No version provided."
   exit 1
-else
-  version=$1
 fi
 
 sed -i "s/version.*/version: '$version',/" ../src/config/docs/basicInfo.ts
 
 npm version $version --git-tag-version false
 
-git add .
-git commit -m "build(release): bump version to v$version"
+[ "$is_commit" = true ] && commit
